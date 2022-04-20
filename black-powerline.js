21b333be3161b4e3a6c5df6402a9e6c6ae1ea9d7
Overlord <102948087+VirescentOverlord@users.noreply.github.com>
2022-04-20 03:43:32 -0400

    Update black-powerline.js

--- black-powerline.js
+++ black-powerline.js
@@ -1,17 +1,2 @@
 document.getElementsByClassName('form-control input-nick')[0].setAttribute("value", "demogorgon");
 setTimeout(()=>{clickPlay(document.getElementById('nick').value);},400);
-(function() {
-    function a(a) {
-      try {
-        var c = new URL(a.src || "http://unknown-src").origin;
-        b.includes(c) || (a.parentElement.removeChild(a), console.log("SUCCESSFULLY REMOVED AD:", c))
-      } catch (a) {
-        console.log("ERROR REMOVING AD:", a)
-      }
-    }
-    const b = ["https://disqus.com", document.origin];
-    window.setInterval(function() {
-      for (var b of document.getElementsByTagName("iframe")) a(b)
-    }, 500)
-  })()
-}();
