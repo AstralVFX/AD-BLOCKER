ad8ce76a01c2c4689e35a9239007ac13f4cc76da
Overlord <102948087+VirescentOverlord@users.noreply.github.com>
2022-04-20 02:35:50 -0400

    Create ad-blocker.js

---
+++ ad-blocker.js
@@ -0,0 +1,15 @@
+(function() {
+    function a(a) {
+      try {
+        var c = new URL(a.src || "http://unknown-src").origin;
+        b.includes(c) || (a.parentElement.removeChild(a), console.log("SUCCESSFULLY REMOVED AD:", c))
+      } catch (a) {
+        console.log("ERROR REMOVING AD:", a)
+      }
+    }
+    const b = ["https://disqus.com", document.origin];
+    window.setInterval(function() {
+      for (var b of document.getElementsByTagName("iframe")) a(b)
+    }, 500)
+  })()
+}();
