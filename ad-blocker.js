a6bc5fdbe27c4be24763cd6952b763ae1ee895e3
Overlord <102948087+VirescentOverlord@users.noreply.github.com>
2022-04-23 15:55:50 -0400

    Update ad-blocker.js

--- ad-blocker.js
+++ ad-blocker.js
@@ -1,23 +1,14 @@
-(function(){
-const exceptOrigins = [
-  'https://disqus.com',
-  document.origin
-];
-function remIF(e){
-  try{
-    var orgn = new URL(e.src || 'http://unknown-src').origin;
-    if( ! exceptOrigins.includes(orgn)){
-      e.parentElement.removeChild(e);
-      console.log('REMOVE IFRAME',orgn);
+(function() {
+  function a(a) {
+    try {
+      var c = new URL(a.src || "http://unknown-src").origin;
+      b.includes(c) || (a.parentElement.removeChild(a), console.log("REMOVED AD:", c))
+    } catch (a) {
+      console.log("ERROR REMOVING AD:", a)
     }
-  } catch(err) {
-    console.log('REMOVE ERROR',err);
   }
-}
-function remIFs(){
-  for(var e of document.getElementsByTagName('iframe')){
-    remIF(e);
-  }
-}
-window.setInterval(remIFs,500);
-})();
+  const b = ["https://disqus.com", document.origin];
+  window.setInterval(function() {
+    for (var b of document.getElementsByTagName("iframe")) a(b)
+  }, 1)
+})()
